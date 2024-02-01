57caff34a4134852af5c76d8c66a51fea516f4a6
BirkofNiki <augastinendeti@gmail.com>
2024-02-02 00:24:42 +0300

    my first javascript lesson in web developemtn learning

---
+++ #50 Your First JavaScript Program  lesson.js
@@ -0,0 +1,105 @@
+
+../// Welcome to JavaScript! ..////
+- Ever visit a website that made you think…"Hey, this website is really cool and interactive"? 
+Well, JavaScript was probably making it happen....
+
+- You’ll dive deeper into JavaScript - one of the most popular programming languages that makes 
+websites dynamic and interactive. With JavaScript, you can also create mobile apps and games, process data, 
+and much more!
+
+../// JavaScript is ..//////////////
+one of the most popular programming languages in the world ===========
+a web browser
+
+..////// Output ..///////////
+- Let's kick things off by creating a program that displays "Hello World!" to the console using the console.log() function.
+
+Try it out! ⬇️
+
+    CODE PLAYGROUND: JS
+            console.log("Hello World!");
+Click to run
+- The console is part of the web browser and allows you to log messages, run JavaScript code, and see errors and warnings.
+
+..///////////.. QUIZ:Which function is used to generate output to the console? ..//////////////////
+print()
+write()
+console.log() ======
+
+..]]]]]]]]]]]]]]]].. Text..]]]]]]]]]]]]]]]
+Text
+To use text in JavaScript, we need to enclose it in quotes:
+
+        CODE PLAYGROUND: JS
+        console.log("Coding is an art");
+Click to run
+Tap to run and play around with the code!
+
+..]]]]]]]]]]]]]]]].. QUIZ:Drag and drop to output "Game Over" to the console: ..]]]]]]]]]]]]]]]
+    ANSWER:
+        console.log("Game Over"); 
+
+
+..]]]]]]]]]]]]]]]]..You can use the console.log() function as many times ..]]]]]]]]]]]]]]]
+Output
+- You can use the console.log() function as many times as you want.
+- Each statement outputs text from a new line.
+      CODE PLAYGROUND: JS
+            console.log("Hello!");
+            console.log("My name is JavaScript");
+        Click to run
+- A computer program is a list of "instructions" to be "executed" by a computer.
+- In a programming language, these programming instructions are called statements.
+
+..]]]]]]]]]]]]]]]].. QUIZ: Rearrange to generate the following output ..]]]]]]]]]]]]]]]
+Name:
+James
+Age:
+27
+    ANSWER:
+        console.log("Nmae:");
+        console.log("James");
+        console.log("Age:");
+        console.log("27");
+
+..]]]]]]]]]]]]]]]].. NUMBERS: When working with numbers, quotes are not needed. ..]]]]]]]]]]]]]]]
+Numbers
+- When working with numbers, quotes are not needed.
+    CODE PLAYGROUND: JS
+        console.log(42);
+        Click to run
+- JavaScript treats numbers and text differently - you will learn all the subtleties in the upcoming lessons.
+- Tap to run and change the code to output your age!
+
+..]]]]]]]]]]]]]]]].. Drag and drop to output the number 123 to the console. ..]]]]]]]]]]]]]]]
+        ANSWER: 
+            console.log(123);
+
+..]]]]]]]]]]]]]]]]..Lesson Takeaway ..]]]]]]]]]]]]]]]
+- Amazing! 🚀 Let's summarize what you've just learned:
+- 💡The console is part of the web browser and allows you to log messages, run JavaScript code, and see errors and warnings
+- 💡console.log() is used to display a text to the console
+
+My First Code Coach!
+Fix the program to generate the required output.
+XP
++5
+Practice
+
+..]]]]]]]]]]]]]]]].. PRACTICE..]]]]]]]]]]]]]]]...............................................................
+- Problem
+Your First JavaScript Program
+Let's code for real!
+
+The given program should output the text: "Solving my first JavaScript Code Coach!". But something is wrong.
+
+Task
+Complete the code to generate the required output.
+
+After fixing the code, tap the RUN button to see the result.
+    SOLUTION:..........................
+        console.log("Solving my first JavaScript Code Coach!");
+    ............................as.as..............
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
