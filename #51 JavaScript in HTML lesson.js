bbf8a637cf4a802d5b6f859c42fa5959200a0347
BirkofNiki <augastinendeti@gmail.com>
2024-02-02 12:21:15 +0300

    added skills on JavaScript in HTML , like the alert box that appears as a pop-up

---
+++ #51 JavaScript in HTML lesson.js
@@ -0,0 +1,112 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..JavaScript in HTML ..]]]]]]]]]]]]]]]
+JavaScript in HTML
+- You can add JavaScript code in an HTML document using the <script> tag.
+- Do you remember the console.log() function from the previous lesson? 
+- Let's see how it's written in the HTML document:
+    CODE:
+        <body>
+            <script>
+            console.log("JavaScript in HTML");
+            </script>
+        </body>
+
+..]]]]]]]]]]]]]]]].. Which HTML tag is used for JavaScript code?..]]]]]]]]]]]]]]]
+    CODE: 
+        <p>
+        <script> =========
+        <img></img>
+
+..]]]]]]]]]]]]]]]]..The Code Playground ..]]]]]]]]]]]]]]]
+- The Code Playground
+- It's likely that you have already run code in our Code Playground. Well, now it's time to talk about it a bit more.
+- The Code Playground is an environment where you can write, run and save your codes and projects that are written in different programming languages - all in one place. Isn't it awesome?
+- In this course, we will use the Web Code Playground. It has three sections for the three core web technologies - HTML, CSS, and JavaScript.
+- Here is an example of code written in the JavaScript section:
+    CODE PLAYGROUND: JS
+        console.log("Welcome to the Code Playground!");
+Click to run
+
+..]]]]]]]]]]]]]]]].. Alert Box..]]]]]]]]]]]]]]]
+- Another way to display messages is an alert box.
+- You can generate them by using the alert() function:
+    - CODE PLAYGROUND: JS
+        alert("Accept multiple downloads");
+Click to run
+- Just like with the console.log(), here the text should be enclosed in quotes.
+
+..]]]]]]]]]]]]]]]]..Fill in the blanks to generate an alert box with the given text: ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        alert("Hi there!");
+
+..]]]]]]]]]]]]]]]]..Comments in JavaScript ..]]]]]]]]]]]]]]]
+- Comments are explanatory statements that you can include in a program to benefit the person reading your code.
+- A single-line comment starts with //:
+CODE PLAYGROUND: JS
+        // outputs a wish
+        console.log("Have a good day!");
+- Click to run
+- The compiler ignores everything that appears in the comment, so none of that information affects the result.
+
+..]]]]]]]]]]]]]]]]..QUIZ: Which of the following is a valid JavaScript single comment? ..]]]]]]]]]]]]]]]
+CHOICES: 
+    # what about me?
+    \\ that's me
+    // choose me =========
+    <!-- maybe me? --></script>
+
+..]]]]]]]]]]]]]]]]..Multiple Comments in Js..]]]]]]]]]]]]]]]
+Comments
+- You can also create multi-line comments.
+- They start with /* and end with */, making everything in between a comment.
+For example:
+    CODE PLAYGROUND: JS
+        /* an important
+        reminder
+        for you */
+        console.log("Keep rocking!");
+Click to run
+
+..]]]]]]]]]]]]]]]]..Drag and drop to make the first two lines a comment. ..]]]]]]]]]]]]]]]
+/*</p>some demo code 
+console.log("Demo"); */
+console.log("Hey there");
+
+..]]]]]]]]]]]]]]]]..Lesson takeaways ..]]]]]]]]]]]]]]]
+- You are fantastic! 🚀 Let's summarize what you've just learned:
+- 💡you can add JavaScript code to an HTML document using the <script> tag.
+- 💡Code Playground is the place where you can make real magic - write, run, save and share your codes! ✨
+- 💡alert() generates an alert box with a message
+- 💡comments are used to explain the code 
+What's next?
+- In the next lesson, you will learn how JavaScript can help you to perform mathematical operations. See you there!
+
+
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
---
+++ #52 AI test for JS.js
@@ -0,0 +1,46 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]..-How do you send a specified message to the console in JavaScript?-..]]]]]]]]]]]]]]]
+CHOICES:
+    using the alert() function
+    using the console.log() function =============
+    using the write.console() function
+
+..]]]]]]]]]]]]]]]].. Which tag needs to be used to add JavaScript code in an HTML document?..]]]]]]]]]]]]]]]
+    CHOICES: 
+        js
+        js-html
+        javascript
+        script ==========
+..]]]]]]]]]]]]]]]]..Drag and drop to complete the following statement. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        console.log("JavaScript is fun");
+
+..]]]]]]]]]]]]]]]]..Drag and drop to display a message in the alert box. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        alert("This is an alert message");
+
+..]]]]]]]]]]]]]]]]..Drag and drop to comment the lines below together. ..]]]]]]]]]]]]]]]
+ ANSWER: 
+    /*
+    Can you
+    comment
+    these lines? */
+
+..]]]]]]]]]]]]]]]]..Rearrange the elements to put them in the correct order for console log function. ..]]]]]]]]]]]]]]]
+    ANSWER:
+        console.log("")
+
+..]]]]]]]]]]]]]]]]..Fill in the blank to execute the following string: ..]]]]]]]]]]]]]]]
+    ANSWER:
+        console.log("JavaScript isn't a car");
+
+..]]]]]]]]]]]]]]]]..Fill in the blank to comment out the line below. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        // Comment this line
+
+..]]]]]]]]]]]]]]]]..Drag and drop to finish the sentence. ..]]]]]]]]]]]]]]]
+ANSWER: 
+    In JavaScript symbols /* and */ are used to comment many lines together.
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
