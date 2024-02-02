6051f2e87b0a536526c3b73a35decc7ba9593d39
BirkofNiki <augastinendeti@gmail.com>
2024-02-02 18:04:18 +0300

    added simple operations skills for Web Development, like division, multiplication, addition, and subtraction, and known that js follows BODMAS rule

---
+++ #53 Simple Operations lesson.js
@@ -0,0 +1,124 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Simple Operations ..]]]]]]]]]]]]]]]
+- Simple Operations
+- Let's talk about calculations! They are everywhere, including in programming. 
+    So, they will be in your future programs too!
+- So let's get prepared for them! 😉
+- Performing a calculation in JavaScript is simple, just enter it into the console.log() function:
+    CODE PLAYGROUND: JS
+        console.log(1 + 4);
+        console.log(6 - 3);
+    Click to run
+
+..]]]]]]]]]]]]]]]]..What is the output of this code? ..]]]]]]]]]]]]]]]
+    console.log(5+4);
+        CHOICES:
+        1
+        54
+        9======
+
+..]]]]]]]]]]]..Multiple calculations within one statement ..]]]]]]]]]]]]]]]
+- You can perform multiple calculations within one statement:
+    Js CODE:
+        console.log(5+6-1);
+    Click to run
+
+..]]]]]]]]]]]]]]]]..Multiplication in Js..]]]]]]]]]]]]]]]
+- Multiplication is done using an asterisk symbol * :
+    - Js CODE:
+        console.log(3*2);
+    Click to run
+
+..]]]]]]]]]]]]]]]]..Drag and drop to output the result of multiplying 15 by 3. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        console.log(15*3);
+
+..]]]]]]]]]]]]]]]]..Division in Js ..]]]]]]]]]]]]]]]
+- Division is done using a slash:
+    JS CODE:
+        console.log(42/7);
+    Click to run
+- Open the Code Playground and perform some calculations!
+
+..]]]]]]]]]]]]]]]]..Let's make sure you haven't forgotten how to output text. ..]]]]]]]]]]]]]]]
+- Complete the code to display "Repetition is the mother of learning".
+        ANSWER: 
+            console.log("Repetition is the mother of learning");
+
+..]]]]]]]]]]]]]]]]..Js uses BODMAS for multiple calculations..]]]]]]]]]]]]]]]
+Simple Operations
+- Just like in regular math, multiplication and division is calculated before addition and subtraction:
+    Js CODE:
+        //2*3 will be calculated first
+        console.log(5+2*3); 
+    Click to run
+
+..]]]]]]]]]]]]]]]]..-What is the output of this code? ..]]]]]]]]]]]]]]]
+Be attentive!
+    console.log(2+2*3);
+    CHOICES:
+        6
+        7
+        8 ======
+
+..]]]]]]]]]]]]]]]].. Controling precedence of Simple Operations..]]]]]]]]]]]]]]]
+Simple Operations
+- To control precedence, use parentheses to indicate the order in which you want to perform operations, like this:
+CODE PLAYGROUND: JS
+    //5+2 will be calculated first
+    console.log((5+2)*3);
+Click to run
+- Make sure to close all your parentheses.
+
+..]]]]]]]]]]]]]]]]..Fill in the blanks to output the result of dividing 55 by 5. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        console.log(55/5);
+
+..]]]]]]]]]]]]]]]]..PRACTICE ..]]]]]]]]]]]]]]]
+        Lesson Takeaways
+        Amazing 🚀!
+
+        Let's summarize what you've just learned:
+
+        💡calculations can be done directly in the console.log() function
+
+        💡multiplication and division have higher precedence than addition and subtraction have
+
+        💡you can control the precedence using parentheses
+
+
+        In the next lesson, you will learn about another key concept in programming - variables!
+
+        widgetImg
+        How Many Seconds?
+        Write a program to calculate how many seconds there are in a month.
+        XP
+        +5
+        Practice
+..]]]]]]]]]]]]]]]]..PRACTICE ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..START OF PRACTICE ..]]]]]]]]]]]]]]]
+Simple Operations
+Ever wondered how many seconds there are in a month (30 days)?
+
+Write a program to calculate and output the answer.
+
+
+Hint
+Remember, there are 24 hours in a day, 60 minutes in an hour, and 60 seconds in a minute.
+
+Use the multiplication operator * inside the console.log() function.
+..]]]]]]]]]]]]]]]]..END OF PRACTICE ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+   ANSWER: 
+    /* 30 days
+    1 day = 24 hours
+    1 hour = 60 minutes
+    1 minute = 60 seconds */
+    //your code goes here
+        console.log(60*60*24*30);
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
