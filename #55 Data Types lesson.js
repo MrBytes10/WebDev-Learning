aeb2c3482af69242822982b1d482c9b2df68591e
BirkofNiki <augastinendeti@gmail.com>
2024-02-04 23:25:07 +0300

    added skills on Javascript datatypes

---
+++ #55 Data Types lesson.js
@@ -0,0 +1,111 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Data Types ..]]]]]]]]]]]]]]]
+- You are making great progress!
+- Another important concept in programming is data types.
+- The term 'data type' refers to the types of values a program can work with.
+- We've already gotten familiar with two types of data: text and number.
+- Text in quotes, like "Hello World" is called a string.
+    CODE PLAYGROUND: JS
+        let msg = "I am a string";
+        let msg2 = 'I am a string as well';
+        console.log(msg);
+        console.log(msg2);
+    Click to run
+- As we have seen, we can use single or double quotes to create strings.
+
+..]]]]]]]]]]]]]]]]..Everything in quotes is a string, even numbers. ..]]]]]]]]]]]]]]]
+- Everything in quotes is a string, even numbers.
+For example:
+    Js CODE:
+        console.log("42");
+    Click to run
+- In the code above, 42 is a string, not a number, which means that the program will treat it as a text consisting of two symbols: "4" and "2".
+
+..]]]]]]]]]]]]]]]]..What is the data type of the given value? ..]]]]]]]]]]]]]]]
+    let myNum = "14"; 
+    CHOICES:
+    number
+    string ===
+
+..]]]]]]]]]]]]]]]]..Numbers ..]]]]]]]]]]]]]]]
+- Numbers can be written with or without decimals.
+    CODE PLAYGROUND: JS
+            let x = 5; //whole number
+            let y = 8.45; //decimal
+        Click to run
+- Use dot . for decimals.
+
+..]]]]]]]]]]]]]]]]..QUIZ: Fill in the blanks to create the "pi" constant and assign it the value 3.14. ..]]]]]]]]]]]]]]]
+    ANSWER: 
+        const pi = 3.14;
+
+..]]]]]]]]]]]]]]]]..You can add, subtract and multiply numbers, producing another number as a result. ..]]]]]]]]]]]]]]]
+Some examples:
+    CODE PLAYGROUND: JS
+        let x = 54;
+        let y = 6;
+        console.log(x*y);
+        console.log(x/y);
+    Click to run
+
+..]]]]]]]]]]]]]]]]..QUIZ: Drag and drop to calculate and output the division of x by y: ..]]]]]]]]]]]]]]]
+ANSWER: 
+    let x = 16;
+    let y= 4;
+    console.log(x/y);
+
+..]]]]]]]]]]]]]]]]..Booleans in js ..]]]]]]]]]]]]]]]
+- Very often, in programming, you will need a data type that can only have one of two values.
+- For example, to create a registration form for a website you may need to have a variable to check whether the user accepted terms and conditions - two possible scenarios: yes or no.
+- For this, JavaScript has the Boolean data type which can only take the values true or false.
+    CODE PLAYGROUND: JS
+        let isActive = true;
+        let isHoliday = false;
+        
+        console.log(isActive);
+        console.log(isHoliday);
+    Click to run
+- There are also numerical representations of Boolean values - know that 1 means true, and 0 is false.
+
+..]]]]]]]]]]]]]]]]..Lesson Takeaways ..]]]]]]]]]]]]]]]
+    -   You are unstoppable!🚀 This was the last lesson of this module, and you nailed it! 💪
+    Here is a quick summary:
+
+        💡the term data type refers to the types of values a program can work with.
+        💡everything in quotes is a string, even numbers
+        💡both whole numbers and decimals in JavaScript belong to the one number data type
+        💡boolean data type is used to only have one of two values: true or false
+
+   CODE:  let IknowDataTypes = true;
+   
+    What's next?
+    In the next lesson, you will learn about operators. Also, you will get a deeper understanding of strings. 
+    See you there!
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
