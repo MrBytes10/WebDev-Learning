57d38df11ae6af59d2046b5473cf9efae34d7379
BirkofNiki <augastinendeti@gmail.com>
2024-02-05 00:06:29 +0300

    conducted a simple task on the module in js

---
+++ #56 Module 1 Quiz  4 .js
@@ -0,0 +1,26 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Which function is used to generate output to the console? ..]]]]]]]]]]]]]]]
+    print()
+    console.log() =====
+    write()
+
+..]]]]]]]]]]]]]]]]..Drag and drop to output the result of dividing 15 by 3. ..]]]]]]]]]]]]]]]
+    ANSWER: console.log(15/3);
+
+..]]]]]]]]]]]]]]]]..Declare a variable called "message" and assign to it the given message. ..]]]]]]]]]]]]]]]
+ANSWER: let message="Finishing the first module!";
+
+..]]]]]]]]]]]]]]]]..True or False? Constants can't change their value after initialization. ..]]]]]]]]]]]]]]]
+    False
+    True =========
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
