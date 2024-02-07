c8d600e033554eb0b14041ee2e4e590bb376a7ac
BirkofNiki <augastinendeti@gmail.com>
2024-02-07 22:45:49 +0300

    Added skills on concatenation, which involves adding two strings to get a single figure/ word

---
+++ #62 Concatenation lesson.js
@@ -0,0 +1,146 @@
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Concatenation..]]]]]]]]]]]]]]]
+- Not only can we add numbers, but we can also add strings, using something called concatenation, which can be done on any two or more strings.
+- For example:
+    CODE PLAYGROUND: JS
+        console.log("Java" + 'Script');
+    Click to run
+- As you can see, the addition operator + is used for concatenation.
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+What is the output of this code?
+    console.log("Awe" + "some");
+JS
+Awe+some
+error
+Awesome =====
+Explanation: 
+Answer is Awesome.
+        When you use the + operator with strings in JavaScript, it concatenates (joins) them. So it is taking the string "Awe" and concatenating it with the string "some", resulting in the combined string "Awesome".
+        This is known as string concatenation. Since there are two string values being joined together, it outputs a single combined string.
+
+..]]]]]]]]]]]]]]]]..Strings containing numbers are still added as strings rather than numbers. ..]]]]]]]]]]]]]]]
+- Strings containing numbers are still added as strings rather than numbers.
+Like this:
+CODE PLAYGROUND: JS
+    console.log("2" + "2");
+Click to run; and the answer is; 
+        22
+
+..]]]]]]]]]]]]]]]]..What is the output of this code? ..]]]]]]]]]]]]]]]
+CODE:   console.log("4"+"3");
+        JS CHOICES:
+            7
+            error
+            43 ======
+
+..]]]]]]]]]]]]]]]]..Concatenation with variableS: ..]]]]]]]]]]]]]]]
+- You can perform concatenation with variables as well:
+    CODE PLAYGROUND: JS
+        let x = "Java";
+        let y = "Script";
+        let z = x +y;//"Java" + "Script"
+        console.log(z);
+    Click to run, and the answer is; 
+            JavaScript
+
+..]]]]]]]]]]]]]]]]..Consistency=Success. Output it! ..]]]]]]]]]]]]]]]
+    CODE: 
+        let x = 'Consistency';
+        let y = '=';
+        let z = 'Success';
+        console.log(x + y + z);
+/* 
+The changes made:
+-Declared x, y and z variables with let instead of some being const and some not declared
+- Added quotes around '=' for y to make it a string
+- Added parentheses around the logged output
+- Called console.log() instead of just console. to print the output
+- By concatenating the x, y and z strings with +, it will output "Consistency=Success" 
+*/
+
+..]]]]]]]]]]]]]]]]..If you are making a sentence, don't forget about the spaces: ..]]]]]]]]]]]]]]]
+Concatenation
+- If you are making a sentence, don't forget about the spaces:
+    CODE PLAYGROUND: JS
+        console.log("Hakuna " + "Matata");
+    Click to run
+
+..]]]]]]]]]]]]]]]]..Lesson Takeaways..]]]]]]]]]]]]]]]
+- Let's summarize what you've just learned:
+    💡you can join two or more strings into another using the + operator, that's called concatenation
+    💡you can perform concatenation using variables with string data
+    💡don't forget about the spaces when making a sentence using concatenation
+- Your next Code Coach exercise is waiting to be solved!
+
+Welcome Message
+Complete a program that will greet everyone with a welcome message․
+Practice
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+MOOOOOOOOOOOOOOOOOOOOOOOOOORE:
+FOR AI practice made: 
+1. True or false: With template literals you can create multiline strings without \n.
+False
+True====
+
+2. What do we use to create template literals?
+quotes “ “
+back-ticks `` ======
+backslash \
+dotes . .
+
+3. Drag and drop to comment out the first line and output 815 to the console.
+//Let’s output 815
+console.log("8"+"1" + "5");
+
+4. Drag and drop to display Welcome Ann to the console.
+CODE: 
+    let name= "Ann";
+    console.log( `Welcome, ${name}`);
+5. Fill in the blank to output butterfly.
+CODE:
+    console.log("butter" + "fly");
+6. What is the output of this code?
+CODE:
+    let x = "55";
+    let y = "0";
+    console.log(x + y);
+JS ANSWER: 550
+REASON: The "55" and "0" are strings but not numbers, hence what happens here is Concatenation.
+
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
