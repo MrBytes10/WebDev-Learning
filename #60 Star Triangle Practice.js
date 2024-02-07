96a5464126790f30d3c561dccf5ab066a0c781e1
BirkofNiki <augastinendeti@gmail.com>
2024-02-07 22:02:37 +0300

    Addded skills on template literals that are created using back-ticks, and don't need to escape quoates

---
+++ #60 Star Triangle Practice.js
@@ -0,0 +1,17 @@
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Star Triangle ..]]]]]]]]]]]]]]]
+    Problem
+    Star Triangle
+    - The given code is designed to draw a triangle made out of stars ✨.
+    -But oh no! There's an error in the code such that it outputs all of the stars on one line, instead of on separate lines.
+    Task
+    - Fix the code to output a triangle of stars that has 3 rows.
+    = Important: Remove any extra spaces to match the required output.
+
+    ANSWER: 
+        console.log("*\n**\n***");
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
---
+++ #61 Template Literals lesson.js
@@ -0,0 +1,87 @@
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+
+..]]]]]]]]]]]]]]]]..Template Literals..]]]]]]]]]]]]]]]
+- Template literals are another way to create strings and work with them more flexibly.
+- They use back-ticks `` rather than quotes "" to define a string:
+    JS CODE:
+        let temp = `Hello, user!`;
+        console.log(temp);
+    Click to run,, and the answer is: 
+            Hello, user! 
+
+..]]]]]]]]]]]]]]]]..Which one is a template literal? ..]]]]]]]]]]]]]]]
+    'Coding is an art'
+    "eat, sleep, code, repeat"
+    `Knowledge is power` ===============
+
+..]]]]]]]]]]]]]]]]..you can use both single and double quotes inside a string ..]]]]]]]]]]]]]]]
+Template literals
+- With template literals, you can use both single and double quotes inside a string.
+    CODE PLAYGROUND: JS
+        console.log(`I'm happy, she said "Yes"!`);
+    Click to run, and the answer is; 
+            I'm happy, she said "Yes"! 
+- NOTE: You don't need to escape the quotes inside template literals.
+
+..]]]]]]]]]]]]]]]]..Template literals allow multiline strings, without using \n: ..]]]]]]]]]]]]]]]
+Template Literals
+- Template literals allow multiline strings, without using \n:
+    CODE PLAYGROUND: JS
+        let msg = `Hey! Are you sololearning?
+        Isn't it awesome?`;
+    Click to run
+
+..]]]]]]]]]]]]]]]]..Template literals allow you to use variables inside the strings ..]]]]]]]]]]]]]]]
+    Template Literals
+    - And that's not all!
+    - Template literals allow you to use variables inside the strings. You need to add a dollar sign $ and enclose the variable name in braces {}.
+    - For example:
+    CODE PLAYGROUND: JS
+        let name = "John";
+        let text = `Welcome, ${name}`;
+        console.log(text);
+    Click to run, and the answer is; 
+            Welcome, John
+
+    - The value of the variable will be automatically shown in the output. Magic? No, that's JavaScript.
+
+..]]]]]]]]]]]]]]]]..Lesson Takeaways..]]]]]]]]]]]]]]]
+- Let's summarize what you've just learned:
+
+    💡template literals are created using back-ticks `` 
+
+    💡quotes don't need to be escaped in template literals
+
+    💡template literals allow multiline strings, without using \n
+
+    💡you can use variables inside the string by adding a dollar sign $ and enclosing the variable name in braces {}
+
+- Hands-on practice with template literals is coming up in your next Code Coach exercise!
+
+Country Cards
+Complete the code to return a string in the required format for your country card program.
+XP
++5
+Practice
+
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
+..]]]]]]]]]]]]]]]].. ..]]]]]]]]]]]]]]]
